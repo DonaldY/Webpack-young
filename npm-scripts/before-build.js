80988ceda43321e8f492a812f8ff3323b1c016b6
DonaldY <448641125@qq.com>
2018-03-23 11:28:23 +0800

    add annotation

--- npm-scripts/before-build.js
+++ npm-scripts/before-build.js
@@ -1,6 +1,12 @@
 var fs = require('fs');
 var rimraf = require('rimraf');
 var dirVars = require('../webpack-config/base/dir-vars.config.js');
+
 rimraf(dirVars.buildDir, fs, function cb() {
-  console.log('build目录已清空');
+  console.log('build was cleaned');
 });
+
+// 根据需求，针对某个个别子目录进行删除
+// rimraf(dirVars.buildDir + '/js', fs, function cb() {
+//    console.log('js was cleaned');
+//});
