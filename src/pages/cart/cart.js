1702a4a8821cd349cc0ff6f16d8ef973b2fd8bab
DonaldY <448641125@qq.com>
2018-03-26 21:16:26 +0800

    clean

--- src/pages/cart/cart.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-require('./cart.css');
-require('../../public-resource/iconfont/font-style.css');
-require('../../public-resource/components/common/default.css');
-
-console.log('This is cart');
