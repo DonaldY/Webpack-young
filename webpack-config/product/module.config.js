b9fbd28ca35eaca36c4b0d6979061544c5f64c99
DonaldY <448641125@qq.com>
2018-04-09 11:06:44 +0800

    update product/module.config.js

--- webpack-config/product/module.config.js
+++ webpack-config/product/module.config.js
@@ -1,19 +1,17 @@
 var dirVars = require('../base/dir-vars.config.js');
-
 var ExtractTextPlugin   = require('extract-text-webpack-plugin');
 
 const moduleConfig = require('../inherit/module.config.js');
 
 moduleConfig.rules.push({
-  test: /\.css$/,
- 
-  use: ExtractTextPlugin.extract([
-    {
-      loader: 'css-loader'
-    },
-   
-  ])
+    test: /\.css$/,
+
+    use: ExtractTextPlugin.extract([
+	{
+	    loader: 'css-loader'
+	}
+    ])
 });
 
 
-module.export = moduleConfig;
+module.exports = moduleConfig;
